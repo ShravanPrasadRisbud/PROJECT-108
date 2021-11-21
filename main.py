a0c1c1f4a9964016fb61ad7dc6d8bea3f73ffed2
ShravanPrasadRisbud <73066805+ShravanPrasadRisbud@users.noreply.github.com>
2021-11-21 18:18:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# import plotly.figure_factory as ff
+# import pandas as pd
+# import csv
+
+# df = pd.read_csv("data.csv")
+# fig = ff.create_distplot([df["Mobile Brand"].tolist()], ["Brand"], show_hist=True)
+# fig.show()
+
+import plotly.figure_factory as ff
+import pandas as pd
+import csv
+
+df = pd.read_csv("data.csv")
+fig = ff.create_distplot([df["Mobile Brand"].tolist()], ["Avg Rating"], show_hist=True)
+fig.show()
